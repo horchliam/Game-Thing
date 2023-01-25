af2610b34726d0a6676a63619db87f261affc6d1
horchliam <53147246+horchliam@users.noreply.github.com>
2023-01-24 19:52:33 -0500

    Initial Commit

---
+++ Game-Thing/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Game-Thing
+//
+//  Created by Liam Horch on 1/24/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
