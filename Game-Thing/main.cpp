453dcccc9fe874655f4d16fd1ca7637be59a2d17
horchliam <53147246+horchliam@users.noreply.github.com>
2023-02-03 18:11:45 -0500

    Copied starting code for breakout

--- Game-Thing/main.cpp
+++ Game-Thing/main.cpp
@@ -1,54 +1,118 @@
-// External libraries
+/*******************************************************************
+ ** This code is part of Breakout.
+ **
+ ** Breakout is free software: you can redistribute it and/or modify
+ ** it under the terms of the CC BY 4.0 license as published by
+ ** Creative Commons, either version 4 of the License, or (at your
+ ** option) any later version.
+ ******************************************************************/
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
 // My headers
 #include "Window.hpp"
 #include "WindowManager.hpp"
+#include "game.h"
+#include "resource_manager.h"
 
-// Stds
 #include <iostream>
 
-void processInput(GLFWwindow *window);
-
-// Globals
+// Global window variable
 GLFWwindow* window;
 
-// settings
-const unsigned int SCR_WIDTH = 800;
-const unsigned int SCR_HEIGHT = 600;
+// GLFW function declarations
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
 
-int main()
+// The Width of the screen
+const unsigned int SCREEN_WIDTH = 800;
+// The height of the screen
+const unsigned int SCREEN_HEIGHT = 600;
+
+Game Breakout(SCREEN_WIDTH, SCREEN_HEIGHT);
+
+int main(int argc, char *argv[])
 {
-    WindowManager wm = WindowManager(SCR_WIDTH, SCR_HEIGHT);
+    WindowManager wm = WindowManager(SCREEN_WIDTH, SCREEN_HEIGHT);
     if(!wm.SetupWindow()) { return 0; }
     
     // glad: load all OpenGL function pointers
+    // ---------------------------------------
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
-
-    // render loop
+    
+    glfwSetKeyCallback(window, key_callback);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    
+    // OpenGL configuration
+    // --------------------
+    glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    
+    // initialize game
+    // ---------------
+    Breakout.Init();
+    
+    // deltaTime variables
+    // -------------------
+    float deltaTime = 0.0f;
+    float lastFrame = 0.0f;
+    
     while (!glfwWindowShouldClose(window))
     {
-        // input
-        processInput(window);
-
-        // render
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
-        glfwSwapBuffers(window);
+        // calculate delta time
+        // --------------------
+        float currentFrame = glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
         glfwPollEvents();
+        
+        // manage user input
+        // -----------------
+        Breakout.ProcessInput(deltaTime);
+        
+        // update game state
+        // -----------------
+        Breakout.Update(deltaTime);
+        
+        // render
+        // ------
+        glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        Breakout.Render();
+        
+        glfwSwapBuffers(window);
     }
+    
+    // delete all resources as loaded using the resource manager
+    // ---------------------------------------------------------
+    ResourceManager::Clear();
+    
     glfwTerminate();
     return 0;
 }
 
-void processInput(GLFWwindow *window) {
-    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+    // when a user presses the escape key, we set the WindowShouldClose property to true, closing the application
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
+    if (key >= 0 && key < 1024)
+    {
+        if (action == GLFW_PRESS)
+            Breakout.Keys[key] = true;
+        else if (action == GLFW_RELEASE)
+            Breakout.Keys[key] = false;
+    }
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    // make sure the viewport matches the new window dimensions; note that width and
+    // height will be significantly larger than specified on retina displays.
+    glViewport(0, 0, width, height);
 }
